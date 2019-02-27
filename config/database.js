252bc4e25ccbe0d7f836e91dabe9edc62b87addd
Gavin Michael <gavin.michael.ftw@gmail.com>
2019-02-27 14:57:30 +0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database:'mongodb://localhost:27017/nodekb',
+  database:"mongodb+srv://dbUser:" + process.env.DBPW + "@cluster0-sixwu.mongodb.net/test?retryWrites=true",
   secret: 'yoursecret'
 }
